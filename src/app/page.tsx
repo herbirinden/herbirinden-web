a4b5683b20f9f36f5167e7732be2955031911c25
Serdal Gürtekin <serdargurtekin@gmail.com>
2025-08-23 14:28:38 +0300

    deploy: landing sayfasına logo görseli

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="min-h-screen grid place-items-center bg-white">
-      <Image
-        src="/logo.png"
-        alt="Herbirinden"
-        width={350}
-        height={600}
-        style={{ objectFit: "contain" }}
-        priority
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      {/* Başlık */}
+      <h1>Herbirinden.com</h1>
+
+      {/* Logo */}
+      <img 
+        src="/logo.png" 
+        alt="Herbirinden Logo" 
+        width="300" 
+        style={{ marginTop: "20px" }}
       />
+
+      {/* Alt yazı */}
+      <p style={{ marginTop: "20px", fontSize: "20px" }}>
+        Çok yakında hem web’de, hem de mobil’deyiz 🚀
+      </p>
     </main>
   );
 }
